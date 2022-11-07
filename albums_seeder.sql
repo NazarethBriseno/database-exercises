a245060e2684e7cd5682f7506351d6b376c016fc
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-11-07 11:20:37 -0600

    Filled in albums_seeder with albums

---
+++ albums_seeder.sql
@@ -0,0 +1,21 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, album_name, release_date, sales, genre) VALUES ("Michael Jackson", "Thriller", 1982, 50.2, "Pop, post-disco, funk, rock"),
+    ("AC/DC", "Back in Black", 1980, 30.1, "Hard Rock"),
+    ("Whitney Houston", "The Bodyguard", 1992, 28.7, "R&B, soul, pop, soundtrack"),
+    ("Pink Floyd", "The Dark Side of the Moon", 1973, 24.8, "Progressive Rock"),
+    ("Eagles", "Their Greates Hits(1971-1975)", 1976, 41.2, "Country Rock, Soft Rock, Folk Rock"),
+    ("Eagles", "Hotel California",1976,31.8,"Soft Rock"),
+    ("Shania Twain","Come on Over",1997,29.3,"Soft Rock"),
+    ("Fleetwood Mac","Rumours",1977,29.3,"Soft Rock"),
+    ("Bee Gees","Saturday Night Fever",1977,22.1, "Disco"),
+    ("Led Zeppelin", "Led Zeppelin IV",1971,30.4, "Hard rock, heavy metal, folk rock"),
+    ("Michael Jackson", "Bad",1987, 21.1, "Pop, rhythm and blues, funk and rock"),
+    ("Meat Loaf","Bat out of Hell",1977,22,"Hard rock, glam rock, progressive rock"),
+    ("Alanis Morissette","Jagged Little Pill",1995,25.4,"Alternative Rock"),
+    ("Various Artists","Dirty Dancing",1987,24.1,"Pop, rock, R&B"),
+    ("Celine Dion","Falling into You",1996,21.1,"Pop, Soft Rock"),
+    ("Adele","21",2011,27.1,"Pop, Soul"),
+    ("The Beatles","1",2000,23.2,"Rock"),
+    ("Metallica","Metallica",1991,22.7,"Heavy Metal");
+
