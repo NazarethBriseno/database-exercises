9b3fcb69f9964571e27e446c00d40cf533d7ac3c
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-11-08 09:21:20 -0600

    Updated select exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,8 +2,9 @@ USE codeup_test_db;
 
 SELECT artist, album_name FROM albums WHERE artist = 'Pink Floyd';
 
-SELECT album_name, release_date  FROM albums WHERE album_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT album_name, release_date  FROM albums WHERE album_name LIKE '%Pepp%';
 
-SELECT artist, album_name FROM albums WHERE release_date BETWEEN 1989 AND 2000;
+SELECT 'Albums released in the 1990''s' AS caption, artist, album_name FROM albums WHERE release_date BETWEEN 1989 AND
+    2000;
 
 SELECT artist, album_name FROM albums WHERE genre = "Rock";
